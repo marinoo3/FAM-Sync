9161a120e1f5ef9157f682911cbb51b14f51d6be
Marin <76592936+marinoo3@users.noreply.github.com>
2025-07-17 10:59:52 +0200

    Add files via upload

--- application/modules/event.py
+++ application/modules/event.py
@@ -5,10 +5,11 @@ from datetime import datetime
 
 class Event:
 
-    def __init__(self, client:str, start_date:datetime, end_date:datetime, ca:float, source:str):
+    def __init__(self, client:str, start_date:datetime, end_date:datetime, ca:float, boat:str, source:str):
         
         self.client = client
         self.start_date = start_date
         self.end_date = end_date
         self.ca = ca
+        self.boat = boat
         self.source = source
